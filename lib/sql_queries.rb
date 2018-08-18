e30a2b4c1eabe7d0ce045e2e630d12f736eb24a4
Catherine Salazar <catcreates13@gmail.com>
2018-08-18 23:21:12 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -51,7 +51,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   INNER JOIN pledges
   ON pledges.user_id = users.id
   GROUP BY pledges.user_id
-  ORDER BY(name) ASC
+  ORDER BY(amount) ASC
   "
 end
 
