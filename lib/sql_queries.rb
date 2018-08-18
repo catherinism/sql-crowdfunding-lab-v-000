99b0956082c06c63c122e8ba1cb2438ad2e77097
Catherine Salazar <catcreates13@gmail.com>
2018-08-18 23:43:12 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -63,10 +63,16 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   INNER JOIN pledges
   ON pledges.project_id = projects.id
   WHERE category = 'music'
-
   "
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+  #find books category
+  #find total of pledge amount for books
+  #return books category and total amount 
+  "SELECT category, SUM(amount) FROM projects
+  INNER JOIN pledges
+  ON pledges.project_id = projects.id
+  WHERE category = 'music'
+  "
 end
