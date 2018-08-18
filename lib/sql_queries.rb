2c4ddaad34c611e268bae7c93435bab41dafaad9
Catherine Salazar <catcreates13@gmail.com>
2018-08-18 23:41:31 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -58,7 +58,7 @@ end
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   #find music category
   #find pledge amount
-  #return music category and pledge amounts
+  #return each music category with pledge amount 
   "SELECT category, amount FROM projects
   INNER JOIN pledges
   ON pledges.project_id = projects.id
