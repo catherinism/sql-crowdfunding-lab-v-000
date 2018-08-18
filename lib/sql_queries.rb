af40a8def888ce1b64182fd2130f2eca58980722
Catherine Salazar <catcreates13@gmail.com>
2018-08-18 23:37:03 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -62,8 +62,6 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   "SELECT category FROM projects
   INNER JOIN pledges
   ON pledges.project_id = projects.id
-  GROUP BY pledges.user_id
-  ORDER BY SUM(amount) ASC, name
   "
 end
 
