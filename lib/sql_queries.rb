37ebbc807209de83911bc63341c3dae2c5c60049
Catherine Salazar <catcreates13@gmail.com>
2018-08-18 23:13:49 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -44,7 +44,12 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+  "SELECT title, SUM(amount)-funding_goal FROM projects
+  INNER JOIN pledges
+  ON pledges.project_id = projects.id
+  GROUP BY pledges.project_id
+  HAVING SUM(amount)-funding_goal >= 0
+  "
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
