9f6b44b5d5954cd07cb30b895427a54b7df4e82f
Catherine Salazar <catcreates13@gmail.com>
2018-08-18 22:43:38 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -33,7 +33,7 @@ end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT title FROM projects
-  LEFT JOIN pledges
+  INNER JOIN pledges
   ON pledges.project_id = projects.id
   WHERE pledges.amount >= projects.funding_goal
   "
