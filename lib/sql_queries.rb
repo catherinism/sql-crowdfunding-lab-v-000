c8f733f8f9a17587e69c9609c16c0a9a1ea65859
Catherine Salazar <catcreates13@gmail.com>
2018-08-18 21:08:08 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,7 +10,6 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 "SELECT title FROM projects
 INNER JOIN pledges
 ON pledges.project_id = projects.id
-ORDER BY ASC title
 "
 end
 
