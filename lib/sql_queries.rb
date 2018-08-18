23a954663fa7c77b9c12190b9c432b244be90e9c
Catherine Salazar <catcreates13@gmail.com>
2018-08-18 23:06:06 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -38,7 +38,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   "SELECT title, SUM(pledges.amount) FROM projects
   INNER JOIN pledges
   ON pledges.project_id = projects.id
-  GROUP BY pledges.user_id
+  GROUP BY pledges.project_id
   HAVING pledges.amount >= projects.funding_goal
   "
 end
